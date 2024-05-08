0d1a11859e5ff353df2d84475adf7832fb30cc91
EgeorUsl <EgorEger@proton.me>
2024-05-08 14:16:23 +0800

    Correct

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@
 Root="/usr/local/mgr5"
 mkdir -p $Root
 
-cp ./billmgr_mod_payment.xml $Root/etc/xml/
-cp ./pmpayment.py $Root/paymethods/pmpayment
-cp ./payment.py $Root/cgi/payment
+cp ./billmgr_mod_testpayment.xml $Root/etc/xml/
+cp ./pmtestpayment.py $Root/paymethods/pmtestpayment
+cp ./testpayment.py $Root/cgi/testpayment
